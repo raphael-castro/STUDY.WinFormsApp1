c3722ce905123dad7d6505c04ca48e62f7939c6e
Raphael Castro <rcastro.br@gmail.com>
2024-03-15 22:05:45 +0000

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            txtSenha = new TextBox();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(txtSenha);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(txtUsuario);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(510, 210);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Suporte Online - Formulário de Login";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(399, 151);
+            button2.Name = "button2";
+            button2.Size = new Size(105, 38);
+            button2.TabIndex = 5;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(274, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 38);
+            button1.TabIndex = 4;
+            button1.Text = "Entrar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(6, 105);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(498, 23);
+            txtSenha.TabIndex = 3;
+            txtSenha.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(7, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Senha:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(6, 50);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(498, 23);
+            txtUsuario.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Úsuario:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(534, 234);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Suporte Online";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button2;
+        private Button button1;
+        private TextBox txtSenha;
+        private Label label2;
+        private TextBox txtUsuario;
+        private Label label1;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,26 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
